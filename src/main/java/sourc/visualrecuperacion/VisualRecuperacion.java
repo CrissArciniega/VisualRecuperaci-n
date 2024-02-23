ad4d23a5bb25b861f0c5d0ec67770d1033298f5b
CrissArciniega <cristian.arciniega071@ist17dejulio.edu.ec>
2024-02-23 12:17:35 -0500

    Recuperacion

---
+++ src/main/java/sourc/visualrecuperacion/VisualRecuperacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sourc.visualrecuperacion;
+
+/**
+ *
+ * @author USER
+ */
+public class VisualRecuperacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
